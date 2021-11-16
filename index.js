4707ebf7120b5dc54d759463504f177f46c38d8d
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-11-16 13:27:16 -0600

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-/* Your Code Here */
+// what is the job of this function? is it supposed to return a string, modify an array?
+
+// each function needs a single, clear purpose...............................................
+
+// what data/info does it need in order to do it's job? what arguments, parameters is necessary?
+
+// what am i expecting as a return value? Should be pretty distinct
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -8,6 +14,74 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+
+function createEmployeeRecord(ary) {
+    let record
+    return record = { 
+        firstName: ary[0],
+        familyName: ary[1],
+        title: ary[2],
+        payPerHour: ary[3],
+        timeInEvents: [], 
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arys) {
+    // console.log(createEmployeeRecord)
+    return arys.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(dateStamp){
+    this.timeInEvents.push(createDSObj("TimeIn", dateStamp))
+    return this
+}
+
+function createDSObj(getType, dateStamp) {
+    return {type: getType, date: dateStamp.slice(0,10), hour: parseInt(dateStamp.slice(-4))}
+}
+
+function createTimeOutEvent(dateStamp){
+    this.timeOutEvents.push(createDSObj("TimeOut", dateStamp))
+    return this
+}
+
+function hoursWorkedOnDate(dateYMD){
+    // console.log(dateYMD)
+    const timeIn = this.timeInEvents.find((e) => e.date === dateYMD).hour
+    // console.log(timeIn)
+    const timeOut = this.timeOutEvents.find((e) => e.date === dateYMD).hour
+    // console.log(timeOut)
+    // let hoursWorked = (timeOut - timeIn)/100
+    // console.log(hoursWorked)
+    return (timeOut - timeIn)/100
+}
+
+function wagesEarnedOnDate(dateYMD){
+    // console.log(dateYMD)
+    // console.log(this)
+    const wage = this.payPerHour
+    // console.log(wage)
+    const hoursWorked = hoursWorkedOnDate(dateYMD)
+    // console.log(hoursWorked)
+    return wage * hoursWorked
+}
+
+function wagesEarnedOnDate(obj, dateYMD){
+
+    const wage = obj.payPerHour
+    const hoursWorked = hoursWorkedOnDate(obj, dateYMD)
+    return wage * hoursWorked
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find((record) => record.firstName === firstName)
+}
+
+function calculatePayroll(records){
+    const allPay = (records.map((empl) => {return allWagesFor(empl)}))
+    return allPay.reduce((acc, cv) => acc + cv)
+}
 
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
